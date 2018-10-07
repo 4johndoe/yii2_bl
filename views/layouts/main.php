84494f874c960c02bc40a9be05e5a30a379ae402
bogdan <bogdandemchenko@gmail.com>
2018-10-07 12:44:33 +0300

    comment call AppAssets and add own assets/PublicAsset

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -10,7 +10,7 @@ use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
-AppAsset::register($this);
+//AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
