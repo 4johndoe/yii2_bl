1b3473225ba5f7a0f1091ef9ba34236b03d79da7
bogdan <bogdandemchenko@gmail.com>
2018-10-14 18:28:36 +0300

    index+view link categories

--- views/site/single.php
+++ views/site/single.php
@@ -13,7 +13,7 @@ use yii\widgets\LinkPager;
                     </div>
                     <div class="post-content">
                         <header class="entry-header text-center text-uppercase">
-                            <h6><a href="#"> <?= $article->category->title ?></a></h6>
+                            <h6><a href="<?= Url::toRoute(['site/category', 'id' => $article->category->id]) ?>"> <?= $article->category->title ?></a></h6>
 
                             <h1 class="entry-title"><a href="blog.html"><?= $article->title ?></a></h1>
 
