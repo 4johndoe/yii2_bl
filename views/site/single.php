90d7f6e71d4c580f637eec084c2c761bdfff42c7
bogdan <bogdandemchenko@gmail.com>
2018-10-14 20:21:29 +0300

    fix

--- views/site/single.php
+++ views/site/single.php
@@ -30,7 +30,7 @@ use yii\helpers\Url;
 
                         <div class="social-share">
 							<span
-                                class="social-share-title pull-left text-capitalize">By Rubel On <?= $article->getDate() ?></span>
+                                    class="social-share-title pull-left text-capitalize">By Rubel On <?= $article->getDate() ?></span>
                             <ul class="text-center pull-right">
                                 <li><a class="s-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
                                 <li><a class="s-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
@@ -185,55 +185,6 @@ use yii\helpers\Url;
                 'categories' => $categories
             ]) ?>
 
-
-                                <a href="<?= Url::toRoute(['site/view', 'id' => $article->id]); ?>" class="popular-img"><img src="<?= $article->getImage(); ?>" alt="">
-
-                                    <div class="p-overlay"></div>
-                                </a>
-
-                                <div class="p-content">
-                                    <a href="<?= Url::toRoute(['site/view', 'id' => $article->id]); ?>" class="text-uppercase"><?= $article->title ?></a>
-                                    <span class="p-date"><?= $article->getDate() ?></span>
-
-                                </div>
-                            </div>
-                        <?php endforeach; ?>
-                    </aside>
-
-                    <aside class="widget pos-padding">
-                        <h3 class="widget-title text-uppercase text-center">Recent Posts</h3>
-
-                        <?php foreach($recent as $article):?>
-                            <div class="thumb-latest-posts">
-                                <div class="media">
-                                    <div class="media-left">
-                                        <a href="<?= Url::toRoute(['site/view', 'id' => $article->id]); ?>" class="popular-img"><img src="<?= $article->getImage() ?>" alt="">
-                                            <div class="p-overlay"></div>
-                                        </a>
-                                    </div>
-                                    <div class="p-content">
-                                        <a href="<?= Url::toRoute(['site/view', 'id' => $article->id]); ?>" class="text-uppercase"><?= $article->title ?></a>
-                                        <span class="p-date"><?= $article->getDate(); ?></span>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php endforeach; ?>
-                    </aside>
-
-                    <aside class="widget border pos-padding">
-                        <h3 class="widget-title text-uppercase text-center">Categories</h3>
-                        <ul>
-                            <?php foreach($categories as $category): ?>
-                                <li>
-                                    <a href="#"><?= $category->title ?></a>
-                                    <span class="post-count pull-right"> (<?= $category->getArticlesCount(); ?>)</span>
-                                </li>
-                            <?php endforeach; ?>
-                        </ul>
-                    </aside>
-
-                </div>
-            </div>
         </div>
     </div>
 </div>
