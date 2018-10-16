9e17627b4e12bba9a3b883d350b63a85e8f6eaea
bogdan demchenko <bogdandemchenko@gmail.com>
2018-10-16 18:25:51 +0300

    vk auth

--- views/auth/login.php
+++ views/auth/login.php
@@ -47,5 +47,18 @@ $this->params['breadcrumbs'][] = $this->title;
                 To modify the username/password, please check out the code <code>app\models\User::$users</code>.
             </div>
         </div>
+
+        <div class="col-md-2">
+            <script type="text/javascript" src="https://vk.com/js/api/openapi.js?159"></script>
+            <script type="text/javascript">
+                VK.init({apiId: 6722945});
+            </script>
+
+            <!-- VK Widget -->
+            <div id="vk_auth"></div>
+            <script type="text/javascript">
+                VK.Widgets.Auth("vk_auth", {"authUrl":"/auth/login-vk"});
+            </script>
+        </div>
     </div>
 </div>
